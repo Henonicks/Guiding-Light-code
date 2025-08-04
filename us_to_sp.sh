24d52ebe183bc303570d36d3741f62bc40a576f8
Henonicks <sawsawsa777@gmail.com>
2025-08-04 23:14:24 +0300

    oops forgot to include the release database

--- us_to_sp.sh
+++ us_to_sp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sqlite3 database/dev.db "UPDATE jtc_default_values set name = REPLACE(name, '_', ' ');"
-# sqlite3 database/release.db "UPDATE jtc_default_values set vc_limit = REPLACE(vc_limit, 100, 0);"
+sqlite3 database/release.db "UPDATE jtc_default_values set name = REPLACE(name, '_', ' ');"
