be2eb12b6a27c554d29aab3dcc2ac636bfee3073
Henonicks <sawsawsa777@gmail.com>
2025-08-20 21:23:55 +0300

    Made a wrapper for the database class so that it doesn't throw whenever it wants to, fixed the select command

--- include/guiding_light/database.hpp
+++ include/guiding_light/database.hpp
@@ -1,20 +1,50 @@
 #ifndef DATABASE_H
 #define DATABASE_H
 
+#include <map>
 #include <set>
 #include "sqlite_modern_cpp.h"
-#include "sqlite_modern_cpp/log.h"
 
 namespace db {
 	using TINYINT = int16_t; // bro why is there no int8_t which isn't a char in c++ twin icl ts pmo lwk 💔🥀
 	using SMALLINT = int16_t;
-	using MEDIUMINT = int;
+	using MEDIUMINT = int; // 24-bit ints also don't exist i think 💔🥀
 	using BIGINT = unsigned long long;
 	// SQL types that I use
 
+	extern std::string_view LOCATION;
+	extern std::string_view SELECT_LOCATION;
 	extern const std::set <std::string> table_names;
 
-	extern sqlite::database sql;
+	class wrapper : public sqlite::database {
+	public:
+		class operator_with_error {
+		public:
+			wrapper& _wrapper;
+			operator_with_error() = delete; // the whole point of this class is to have a ref to the parent
+			explicit operator_with_error(wrapper&);
+			sqlite::database_binder operator <<(sqlite::str_ref query) const;
+		};
+		using database::database;
+		using database::operator=;
+
+		operator_with_error with_error{*this};
+
+		/**
+		 * @brief Handle an SQL query and catch any exceptions on failure.
+		 * @param query The SQL query to handle.
+		 * @return I've honestly got no clue what a database_binder is
+		 */
+		sqlite::database_binder operator <<(sqlite::str_ref query);
+	};
+
+	// A map with true/false values representing the existence of potential
+	// pending errors, caught during an SQL query in functions.
+	extern std::map <std::string, bool> errors_pending;
+
+	std::string line_comment(std::string_view comment);
+
+	extern wrapper sql;
 
 	/**
 	 * @brief Tries to connect to the database and reports whether the connection was successful.
--- src/lib/database.cpp
+++ src/lib/database.cpp
@@ -1,17 +1,58 @@
 #include "guiding_light/database.hpp"
 #include "guiding_light/configuration.hpp"
 
-sqlite::database db::sql{""};
+std::string_view db::LOCATION = "../database";
+std::string_view db::SELECT_LOCATION = "../database/select";
 const std::set <std::string> db::table_names =
 {"jtc_vcs", "temp_vc_notifications", "jtc_default_values", "no_temp_ping", "topgg_guild_choices", "topgg_guild_votes_amount", "no_noguild_reminder", "topgg_notifications", "tickets", "temp_vcs"};
+db::wrapper db::sql{""};
+std::map <std::string, bool> db::errors_pending;
+
+db::wrapper::operator_with_error::operator_with_error(wrapper& _wrapper) : _wrapper(_wrapper) {}
+
+sqlite::database_binder db::wrapper::operator_with_error::operator <<(sqlite::str_ref query) const {
+	return _wrapper << query;
+}
+
+sqlite::database_binder db::wrapper::operator <<(sqlite::str_ref query) {
+	try {
+		// try to return the result of the original operator
+		return database::operator <<(query);
+	}
+	catch (const sqlite::sqlite_exception& e) {
+		// catch an exception if there is, log it
+		// if there's a comment, see where we came from
+		const size_t pos = query.find("--");
+		std::string function;
+		if (pos != std::string::npos) {
+			function = query.substr(pos + 3).data();
+			if (errors_pending[function]) {
+				errors_pending[function] = false;
+				std::cerr << fmt::format(
+					"SQL errors occurred. Check logging/{0}/{1}/sql_logs.log for more info.\n",
+					IS_CLI ? "cli" : "bot", MODE_NAME
+				);
+			}
+		}
+		sql_log(e, function);
+	}
+	return database::operator <<(";");
+}
+
+std::string db::line_comment(std::string_view comment) {
+	return fmt::format(" -- {}", comment);
+}
 
 bool db::connection_successful() {
-	sqlite::error_log([](const sqlite::sqlite_exception& e) {
-		sql_log(e);
-	});
+	const std::filesystem::path db_path(LOCATION);
+	if (!std::filesystem::exists(db_path)) {
+		std::filesystem::create_directory(db_path);
+	}
 	sql = sqlite::database(fmt::format("../database/{}.db", MODE_NAME));
+	// for some reason if I try to assign an instance of wrapper it tells me that the operator= is deleted
+	// but then why can I do this? I'm confused.
 	try {
-		sql << "SELECT 1 FROM jtc_vcs;"; // try to select a random value from jtc_vcs. If the table doesn't exist, we need to handle an exception.
+		sql.with_error << "SELECT 1 FROM jtc_vcs;"; // try to select a random value from jtc_vcs. If the table doesn't exist, we need to handle an exception.
 	}
 	catch (const std::exception& e) { // wait till the user finds out we're gonna crash their shi 🔥
 		log(fmt::format("Error connecting to the database: {}", e.what()));
